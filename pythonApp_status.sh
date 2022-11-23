dda023f6e07d050228cec99a59cde2394b59db2b
Afroz Basha Shaik Kanala <80997964+afrozbasha@users.noreply.github.com>
2022-11-23 12:47:46 +0530

    Create pythonApp_status.sh

---
+++ pythonApp_status.sh
@@ -0,0 +1,9 @@
+status=$(helm status backend | grep "STATUS")
+
+case "$status" in
+	#case 1
+	"STATUS: deployed") helm status backend | grep "STATUS" ;;
+	
+	#case 2
+	"STATUS: pending" | "STATUS: unknown" | "STATUS: uninstalled" | "STATUS: superseded" | "STATUS: failed" | "STATUS: uninstalling" | "STATUS: pending-install" | "STATUS: pending-upgrade" | "STATUS: pending-rollback") helm rollback backend ;;
+esac
