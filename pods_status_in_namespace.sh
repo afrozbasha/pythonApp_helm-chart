7a188b38b8e06c100249c7bd9652b7d875e8e28e
Afroz Basha Shaik Kanala <80997964+afrozbasha@users.noreply.github.com>
2022-11-24 12:46:14 +0530

    Update pods_status_in_namespace.sh

--- pods_status_in_namespace.sh
+++ pods_status_in_namespace.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-kubectl get pods -n python-app -o NAME | awk -F "/" '{print $2}' > file.txt
+kubectl get pods -n jenkins -o NAME | awk -F "/" '{print $2}' > file.txt
 file='file.txt'
 i=1
 check="Status:       Running"
 while read pod; do
-status=$(kubectl describe pod -n python-app $pod | grep "Status:")
+status=$(kubectl describe pod -n jenkins $pod | grep "Status:")
 echo "pod No.$i $pod -> $status"
 
 case "$status" in
 	#case 1
-	"Status:       Running") helm status nginx --namespace python-app | grep "STATUS" ;;
+	"Status:       Running") helm status nginx --namespace jenkins | grep "STATUS" ;;
 	#case 2
-	"Status:       Pending") helm rollback --namespace python-app nginx ;;
+	"Status:       Pending") helm rollback --namespace jenkins nginx ;;
 	#else
-    *) helm rollback --namespace python-app nginx ;;
+    *) helm rollback --namespace jenkins nginx ;;
 esac
 
 i=$((i+1))
