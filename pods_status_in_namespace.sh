7b1dc7388e0bd7cd3f09dd219e2664c1d69c74df
Afroz Basha Shaik Kanala <80997964+afrozbasha@users.noreply.github.com>
2022-11-24 12:35:06 +0530

    Checking Status of Every Pod in K8s Namespace

---
+++ pods_status_in_namespace.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+kubectl get pods -n python-app -o NAME | awk -F "/" '{print $2}' > file.txt
+file='file.txt'
+i=1
+check="Status:       Running"
+while read pod; do
+status=$(kubectl describe pod -n python-app $pod | grep "Status:")
+echo "pod No.$i $pod -> $status"
+
+case "$status" in
+	#case 1
+	"Status:       Running") helm status nginx --namespace jenkins | grep "STATUS" ;;
+	#case 2
+	"Status:       Pending") helm rollback --namespace jenkins nginx ;;
+	#else
+    *) helm rollback --namespace jenkins nginx ;;
+esac
+
+i=$((i+1))
+done < $file
