7b7e12a4a7a0fce2b6b4cf2916e6ad702967c06b
Afroz Basha Shaik Kanala <80997964+afrozbasha@users.noreply.github.com>
2022-12-06 13:51:48 +0530

    Delete pythonApp_status.sh

--- pythonApp_status.sh
+++
@@ -1,8 +0,0 @@
-status=$(helm status backend | grep "STATUS")
-
-case "$status" in
-	#case 1
-	"STATUS: deployed") helm status backend | grep "STATUS" ;;
-	#case 2
-	"STATUS: pending" | "STATUS: unknown" | "STATUS: uninstalled" | "STATUS: superseded" | "STATUS: failed" | "STATUS: uninstalling" | "STATUS: pending-install" | "STATUS: pending-upgrade" | "STATUS: pending-rollback") helm rollback backend ;;
-esac
